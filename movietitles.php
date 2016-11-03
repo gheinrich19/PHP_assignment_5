a732f7bf96735c06e8238a3bbcafcde1c0acb3d5
Gage <gage.heinrich@gmail.com>
2016-11-02 21:51:17 -0600

    first push from new linux install

---
+++ movietitles.php
@@ -0,0 +1,14 @@
+<?php
+// hello world
+
+$movie = "this is the coolest";
+
+$arr = $titles = array("billy bob", "dildy baggins", "mr. pitiful");
+
+
+foreach ($arr as $value) {
+	
+	echo $value . "<br />";
+}
+
+?>
